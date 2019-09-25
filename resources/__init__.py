139f6dad8a02e52e39a5ceee15aa2f9659cb4e19
haqqer <ilhamserbaguna@gmail.com>
2019-09-25 15:28:45 +0700

    change resources folder to api

--- resources/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify, request
-from flask_restful import Resource
-import requests 
-from bs4 import BeautifulSoup
-import json
-from pprint import pprint
-from flask_cors import CORS
-import re
-import os
